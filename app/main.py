dda858dc31b117a5df88c146c07a321d8dd5bcbc
wpn10 <wpn10@iitbbs.ac.in>
2025-01-12 15:18:15 +0530

    Parsing of API versions

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import socket 
 
-def create_message(correlation_id):
+def create_message(correlation_id, error_code):
     id_bytes = correlation_id.to_bytes(4, byteorder='big')
-    return len(id_bytes).to_bytes(4, byteorder='big') + id_bytes
+    error_code_bytes = error_code.to_bytes(2, byteorder='big', signed=True)
+    return len(id_bytes + error_code_bytes).to_bytes(4, byteorder='big') + id_bytes + error_code_bytes
 
 def handle_client(client_socket, client_address):
     # First read the length (4 bytes)
@@ -15,7 +16,16 @@ def handle_client(client_socket, client_address):
     # Correlation ID is at bytes 4-8 of the message
     correlation_id = int.from_bytes(message[4:8], byteorder='big')
     
-    client_socket.sendall(create_message(correlation_id))
+    # Request API version is at bytes 6-8 of the message
+    request_api_version = int.from_bytes(message[6:8], byteorder='big')
+    
+    # Check if the request API version is supported
+    if request_api_version > 4:
+        error_code = 35  # UNSUPPORTED_VERSION
+    else:
+        error_code = 0  # No error
+    
+    client_socket.sendall(create_message(correlation_id, error_code))
     client_socket.close()
 
 def main():
